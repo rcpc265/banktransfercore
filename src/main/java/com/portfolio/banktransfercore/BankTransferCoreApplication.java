4985935ac8d304fd1c885ef59a8668d0fe084637
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2026-05-19 10:55:25 -0500

    chore: initial project structure from spring initializr

---
+++ src/main/java/com/portfolio/banktransfercore/BankTransferCoreApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.banktransfercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransferCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/banktransfercore/BankTransferCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.banktransfercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
