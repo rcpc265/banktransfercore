c6dd59a803f183c8f478edbb7d98d4f07be00ebd
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2026-05-20 11:01:33 -0500

    feat: add ProcessTransferService implementation in application layer

---
+++ src/main/java/com/portfolio/banktransfercore/application/services/ProcessTransferService.java
@@ -0,0 +1,27 @@
+package com.portfolio.banktransfercore.application.services;
+
+import com.portfolio.banktransfercore.application.ports.AccountRepositoryPort;
+import com.portfolio.banktransfercore.domain.Account;
+import java.math.BigDecimal;
+
+public class ProcessTransferService {
+
+    private final AccountRepositoryPort accountRepositoryPort;
+    public ProcessTransferService(AccountRepositoryPort accountRepositoryPort) {
+        this.accountRepositoryPort = accountRepositoryPort;
+    }
+
+    public void execute(String sourceNumber, String destinationNumber, BigDecimal amount) {
+        Account sourceAccount = accountRepositoryPort.findByAccountNumber(sourceNumber)
+                .orElseThrow(() -> new IllegalArgumentException("La cuenta de origen no existe"));
+
+        Account destinationAccount = accountRepositoryPort.findByAccountNumber(destinationNumber)
+                .orElseThrow(() -> new IllegalArgumentException("La cuenta de destino no existe"));
+
+        sourceAccount.debit(amount);
+        destinationAccount.credit(amount);
+
+        accountRepositoryPort.save(sourceAccount);
+        accountRepositoryPort.save(destinationAccount);
+    }
+}
